38215b1218a84caf5b78b216864eb2d8c527d189
nely de faria <nelly.nf54@gmail.com>
2022-02-20 19:54:11 -0300

    Diminuindo o número de variáveis

--- curso-python/exercises/ex005.py
+++ curso-python/exercises/ex005.py
@@ -3,4 +3,4 @@ num = int(input('Digite um número: '))
 antecessor = num - 1
 sucessor = num + 1
 
-print(f'Analisando o valor {num} seu antecessor é {antecessor} e o seu sucessor é {sucessor}.')
+print(f'Analisando o valor {num} seu antecessor é {num - 1} e o seu sucessor é {num + 1}.')
