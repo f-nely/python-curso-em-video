241f2e87b2aca9d3df20bd02c4abac7c7b0ea023
nely de faria <nelly.nf54@gmail.com>
2022-02-20 19:42:01 -0300

    Criando arquivo operadores_aritmeticos.py

---
+++ curso-python/mundo-1/operadores_aritmeticos.py
@@ -0,0 +1,23 @@
+print(5 + 3 * 2)
+print(5 ** 2)
+print(19 // 2)
+print(365 ** 522)
+print(122 % 3)
+print(4 ** 3)
+pow(4, 3)
+print(81 ** (1/2))
+print('Oi' * 5)
+print('=' * 20)
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+divisao = round(d, 2)
+di = n1 // n2
+e = n1 ** n2
+
+print(f'A soma é {s}, o produto é {m} e a divisão é {divisao}')
+print(f'Divisão inteira {di} e potência {e}')
