514424a62ada835e9b8513f6f81c820aa0f7ec0f
nely de faria <nelly.nf54@gmail.com>
2022-02-22 14:59:34 -0300

    Criando arquivo ex020.py

---
+++ curso-python/exercises/ex020.py
@@ -0,0 +1,11 @@
+from random import shuffle
+
+n1 = input('Primeiro aluno: ')
+n2 = input('Segundo aluno: ')
+n3 = input('Terceiro aluno: ')
+n4 = input('Quarto aluno: ')
+
+alunos = [n1, n2, n3, n4]
+shuffle(alunos)
+
+print(f'A ordem de apresentação será \n {alunos}')
