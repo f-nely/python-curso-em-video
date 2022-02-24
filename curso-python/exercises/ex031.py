858c770cc68bceb53a4df7ec999aadd7a0a828db
nely de faria <nelly.nf54@gmail.com>
2022-02-24 17:41:40 -0300

    Criando arquivo ex031.py

---
+++ curso-python/exercises/ex031.py
@@ -0,0 +1,6 @@
+distancia = float(input('Qual é a distância da sua viagem? '))
+
+print(f'Você está prestes a começar uma viagem de Km{distancia}')
+
+if distancia <= 200:
+    print(f'E o preço da sua passagem será de R$ ')
