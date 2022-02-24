e3cda68d97f83472cebf2799e8b2073b626f8627
nely de faria <nelly.nf54@gmail.com>
2022-02-23 21:54:06 -0300

    Criando arquivo condicionais_parte_01.py

---
+++ curso-python/mundo-1/condicionais_parte_01.py
@@ -0,0 +1,19 @@
+# tempo = int(input('Quantos anos tem seu carro? '))
+
+# if tempo <= 3:
+#     print('Carro novo: ')
+# else:
+#     print('Carro velho: ')
+
+# print('Carro novo' if tempo <= 3 else'Carro velho')
+#
+# print('--FIM--')
+
+name = str(input('Qual é seu nomes? '))
+
+if name == 'Gustavo':
+    print('Que nome lindo você tem!')
+else:
+    print('Seu nome é tão normal!')
+
+print('Boa noite,', name)
