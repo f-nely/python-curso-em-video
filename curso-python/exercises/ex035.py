2f5e9a37c59cd2687fc8996bce7c2c50204aad39
nely de faria <nelly.nf54@gmail.com>
2022-02-25 13:41:22 -0300

    Criando arquivo ex035.py

---
+++ curso-python/exercises/ex035.py
@@ -0,0 +1,8 @@
+l1 = float(input('Primeiro segmento: '))
+l2 = float(input('Segundo segmento: '))
+l3 = float(input('Terceiro segmento: '))
+
+if l1 + l2 > l3 and l2 + l3 > l1 and l1 + l3 > l2:
+    print('Os segmentos acima PODEM FORMAR um triângulo!')
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR um triângulo.')
