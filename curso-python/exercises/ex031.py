9b6fed3ab0250b1586a370d0a76013c01bad0f96
nely de faria <nelly.nf54@gmail.com>
2022-02-25 11:43:58 -0300

    Finalizando exercício

--- curso-python/exercises/ex031.py
+++ curso-python/exercises/ex031.py
@@ -1,6 +1,8 @@
 distancia = float(input('Qual é a distância da sua viagem? '))
 
-print(f'Você está prestes a começar uma viagem de Km{distancia}')
+print(f'Você está prestes a começar uma viagem de {distancia}Km.')
 
-if distancia <= 200:
-    print(f'E o preço da sua passagem será de R$ ')
+if 0 < distancia <= 200:
+    print(f'E o preço da sua passagem será de R$ {distancia * 0.50}')
+else:
+    print(f'E o preço da sua passagem será de R$ {distancia * 0.45}')
