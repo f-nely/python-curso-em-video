8286ef602e2d34689f77e4f4e4aae562c2c1c49f
nely de faria <nelly.nf54@gmail.com>
2022-02-25 13:34:41 -0300

    Criando arquivo ex032.py

---
+++ curso-python/exercises/ex032.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+year = int(input('Que ano quer analisar? Coloque 0 para analisar o ano atual: '))
+
+if year == 0:
+    year = date.today().year
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    print(f'O ano {year} é BISSEXTO')
+else:
+    print(f'Ano {year} não é BISSEXTO')
