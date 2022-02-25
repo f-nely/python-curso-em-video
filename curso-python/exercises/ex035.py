830c47e1f466897befc497434aa81951e5983373
nely de faria <nelly.nf54@gmail.com>
2022-02-25 13:52:21 -0300

    Corrigindo exercício

--- curso-python/exercises/ex035.py
+++ curso-python/exercises/ex035.py
@@ -1,8 +1,13 @@
+
+print('-=' * 20)
+print('Analisandor de Triângulos')
+print('-=' * 20)
+
 l1 = float(input('Primeiro segmento: '))
 l2 = float(input('Segundo segmento: '))
 l3 = float(input('Terceiro segmento: '))
 
-if l1 + l2 > l3 and l2 + l3 > l1 and l1 + l3 > l2:
+if l1 < l2 + l3 and l2 < l3 + l1 and l3 < l1 + l2:
     print('Os segmentos acima PODEM FORMAR um triângulo!')
 else:
     print('Os segmentos acima NÃO PODEM FORMAR um triângulo.')
