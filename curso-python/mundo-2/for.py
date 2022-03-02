4add5791eca85d57c04f32c20a43c343fab83259
nely de faria <nelly.nf54@gmail.com>
2022-03-02 20:38:30 -0300

    Criando arquivo for.py

---
+++ curso-python/mundo-2/for.py
@@ -0,0 +1,10 @@
+# for i in range(6, 0, -1):
+#     print(i)
+# print('FIM')
+
+s = 0
+for i in range(0, 4):
+    n = int(input('Digite um valor: '))
+    s += n
+
+print(f'O somatório de todos os valores foi {s}')
