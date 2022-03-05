3d8301401bca397a0c13a3420cc2b8b8608bd476
nely de faria <nelly.nf54@gmail.com>
2022-03-05 12:34:31 -0300

    Criando arquivo ex055.py

---
+++ curso-python/exercises/ex055.py
@@ -0,0 +1,11 @@
+maior = 0
+menor = 1000
+for i in range(1, 6):
+    peso = float(input(f'Peso da {i}ª pessoa: '))
+    if peso > maior:
+        maior = peso
+    if peso < menor:
+        menor = peso
+
+print(f'O maior peso lido foi de {maior}')
+print(f'O menor peso lido foi de {menor}')
