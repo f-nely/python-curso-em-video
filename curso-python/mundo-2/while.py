913c10894d14abc89e921b8927a5baa74bd50341
nely de faria <nelly.nf54@gmail.com>
2022-03-06 19:18:28 -0300

    Criando arquivo while.py

---
+++ curso-python/mundo-2/while.py
@@ -0,0 +1,27 @@
+# j = 0
+# while j <= 10:
+#     print(j)
+#     j += 1
+# print('FIM')
+
+# n = 1
+# while n != 0:
+#     n = int(input('Digite um valor: '))
+# print('FIM')
+
+# r = 'S'
+# while r == 'S':
+#     n = int(input('Digite um valor: '))
+#     r = str(input('Quer continuar? [S/N] ')).upper()
+# print('FIM')
+
+qtd_par = qtd_impar = 0
+n = 1
+while n != 0:
+    n = int(input('Digite um valor: '))
+    if n % 2 == 0:
+        qtd_par += 1
+    else:
+        qtd_impar += 1
+print(f'Você digitou {qtd_par-1} números pares e {qtd_impar} números ímpares!')
+print('ACABOU')
